8e135cb3a436ea6a2b8df0707e51db2ef663a8bc
Galder Zamarreño <galder@zamarreno.com>
2022-09-30 11:42:25 +0200

    Add substitution to dump unreachable objects

---
+++ quarkus-modules/quarkus-vs-springboot/quarkus-project/src/main/java/com/baeldung/quarkus_project/graal/Target_org_graalvm_nativeimage_VMRuntime.java
@@ -0,0 +1,19 @@
+package com.baeldung.quarkus_project.graal;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+import org.graalvm.nativeimage.ImageSingletons;
+import org.graalvm.nativeimage.impl.HeapDumpSupport;
+
+import java.io.IOException;
+
+@TargetClass(className = "org.graalvm.nativeimage.VMRuntime")
+public class Target_org_graalvm_nativeimage_VMRuntime
+{
+    @Substitute
+    public static void dumpHeap(String outputFile, boolean live) throws IOException
+    {
+        System.out.println("Dump heap with unreachable objects");
+        ImageSingletons.lookup(HeapDumpSupport.class).dumpHeap(outputFile, false);
+    }
+}
